afdc1b8fc34e1ea253b27d84efac8b1c9198616b
multischoolpro <devpisith@gmail.com>
2025-06-19 08:26:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+// server.js
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const userRoutes = require('./routes/users');
 
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});

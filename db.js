ffcaad50ba1bbe348e662c689e5f297b71b2365a
multischoolpro <devpisith@gmail.com>
2025-06-19 08:21:46 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,22 @@
+// db.js
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config();
 
+const sslOptions = process.env.DB_SSL === 'true'
+  ? {
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/ca.pem') // path to your PEM file
+      }
+    }
+  : {};
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  ...sslOptions
+});
+
+module.exports = pool.promise();
